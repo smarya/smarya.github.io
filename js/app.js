784dcd9868a42c7e27d84140dc628f5df2ffc668
smarya <imadurgaprasad@gmail.com>
2018-01-11 10:45:13 +0530

    dding angularjs routing

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module("smarya", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm"
+    })
+    .when("/red", {
+        templateUrl : "red.htm"
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    })
+    .otherwise({
+        template : "404.html"
+    });
+});
