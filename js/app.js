f489dd18b7a5001fd343538355f52e1e00db0bb0
smarya <imadurgaprasad@gmail.com>
2018-01-15 08:00:23 +0530

    changed in files html js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,11 @@ var app = angular.module("smarya", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "main.htm"
+        templateUrl : "index.htm"
     })
     .when("/bhogi", {
         templateUrl : "bhogi.htm"
     })
-   
     .otherwise({
         template : "404.html"
     });
